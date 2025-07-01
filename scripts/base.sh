a9b05438c9c39521a9698ac468dd0a71e7cf6a4a
zennon-sml <zennonsampaioml@gmail.com>
2025-07-01 10:27:55 -0300

    everything

---
+++ scripts/base.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+setxkbmap -layout br &
+
+copyq &
+
+volumeicon &
+
+picom &
+
+#xdotool key super+n &
+
+#alacritty -e sh -c 'neofetch; sleep 1;'
+
---
+++ scripts/wall.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+feh --bg-fill -z ~/Pictures/wall/
